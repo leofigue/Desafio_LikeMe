484ca64e969dda672435c6904f576f16f8e6102a
leofigue <leo.figueroa.a@hotmail.com>
2024-03-09 19:39:56 -0300

    agregue las funciones para actulizar y para eliminar

--- api/models/model.js
+++ api/models/model.js
@@ -2,8 +2,8 @@ const { pool } = require('../database/connection.js');
 
 
 const traePost = async () => {
-    const { rows } = await pool.query("SELECT * FROM posts");
-    console.log(rows);
+    const { rows } = await pool.query("SELECT * FROM posts ORDER BY id");
+    //console.log(rows);
     return rows;
   };
 
@@ -12,7 +12,21 @@ const agregaPost = async (titulo, url, descripcion) => {
   const values = [titulo, url, descripcion]
   const result = await pool.query(consulta, values)
   return result.rowCount
+}; 
+
+const actualizaPost = async ({id}) => {
+  const consulta = "UPDATE posts SET likes=likes+1 WHERE id = $1"
+  const values = [id]
+  const result = await pool.query(consulta, values)
+  return result.rowCount
+};  
+
+const eliminaPost = async ({id}) => {
+  const consulta = "DELETE FROM posts WHERE id = $1"
+  const values = [id]
+  const result = await pool.query(consulta, values)
+  return result.rowCount
 };  
     
 
-module.exports= { traePost, agregaPost };
+module.exports= { traePost, agregaPost, actualizaPost, eliminaPost };
